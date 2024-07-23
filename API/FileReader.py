eb091ef337c32346ede9b3b5565d7dad16ea5510
q33566 <qaz57123@gmail.com>
2024-07-23 11:37:15 +0800

    merge

--- API/FileReader.py
+++ API/FileReader.py
@@ -13,4 +13,5 @@ def readGeneratedRegex(file) -> list:
     data = pd.read_csv(file, usecols=[0],header=None,encoding='ISO-8859-1')
     #data = data.dropna()
     return data.iloc[:, 0].tolist()
+#
     
